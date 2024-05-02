292d613be1c2486c8655d4570c453530601fc018
Curt Warfield <cwarfiel@redhat.com>
2024-05-02 12:48:55 -0400

    update nth_day_cron.sh

--- script/nth_day_cron.sh
+++ script/nth_day_cron.sh
@@ -4,7 +4,7 @@ clear
 intro_message=$(cat <<EOF
 Cron scheduling for "nth day" of the month.
 
-When scheduling tasks for specific dates like the 3rd Tuesday or 2nd Friday of the month, there's no direct built-in method available in cron.
+When scheduling tasks for specific dates like the 3rd tuesday or 2nd friday of the month, there's no direct built-in method available in cron.
 Instead, you'll have to employ shell scripting to accomplish this.
 
 This script will automatically output the line that can be used for the crontab entry.
